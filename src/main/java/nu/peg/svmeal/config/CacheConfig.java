5de1d75bc61c4e9a542c7224ce856f14f9b76c69
Joel Messerli <git@pegnu.dev>
2023-10-31 09:52:29 +0100

    chore: spring boot 3, replace cache implementation

---
+++ src/main/java/nu/peg/svmeal/config/CacheConfig.java
@@ -0,0 +1,43 @@
+package nu.peg.svmeal.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CacheConfig {
+  private final CacheProperties cacheProperties;
+
+  @Bean
+  public CaffeineCache restaurantsCache() {
+    return new CaffeineCache(
+        CacheNames.RESTAURANTS,
+        Caffeine.newBuilder()
+            .maximumSize(cacheProperties.restaurants().maxSize())
+            .expireAfterWrite(cacheProperties.restaurants().expireAfterWrite())
+            .build());
+  }
+
+  @Bean
+  public CaffeineCache restaurantDtos() {
+    return new CaffeineCache(
+        CacheNames.RESTAURANT_DTOS,
+        Caffeine.newBuilder()
+            .maximumSize(cacheProperties.restaurantDtos().maxSize())
+            .expireAfterWrite(cacheProperties.restaurantDtos().expireAfterWrite())
+            .build());
+  }
+
+  @Bean
+  public CaffeineCache mealplan() {
+    return new CaffeineCache(
+        CacheNames.MEAL_PLAN,
+        Caffeine.newBuilder()
+            .maximumSize(cacheProperties.mealPlan().maxSize())
+            .expireAfterWrite(cacheProperties.mealPlan().expireAfterWrite())
+            .build());
+  }
+}
--- src/main/java/nu/peg/svmeal/config/CacheNames.java
+++ src/main/java/nu/peg/svmeal/config/CacheNames.java
@@ -1,5 +1,10 @@
 package nu.peg.svmeal.config;
 
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
 public final class CacheNames {
   public static final String RESTAURANTS = "restaurants";
   public static final String RESTAURANT_DTOS = "restaurantDtos";
