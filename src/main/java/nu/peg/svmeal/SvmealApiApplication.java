e7989c6732323fd1386cabb066cf99aae3b83d7d
Joel Messerli <git@pegnu.dev>
2023-10-31 09:52:29 +0100

    chore: spring boot 3, replace cache implementation

--- src/main/java/nu/peg/svmeal/SvmealApiApplication.java
+++ src/main/java/nu/peg/svmeal/SvmealApiApplication.java
@@ -1,11 +1,14 @@
 package nu.peg.svmeal;
 
+import nu.peg.svmeal.config.CacheProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableConfigurationProperties(value = {CacheProperties.class})
 public class SvmealApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(SvmealApiApplication.class, args);
